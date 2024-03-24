73b098d277b4d60cad659805d7e1f3ddc0aa309d
Léo Geison <leogeisom@gmail.com>
2024-03-24 17:43:02 -0300

    code review and adjustment

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
+//Grafico de linha
 Highcharts.chart('whasAppChart', {
   chart: {
-    type: 'spline',
-    
-    
+    type: 'spline'
   },
   title: {
     text: 'WhatsApp Últimas 24 Horas',
@@ -74,73 +73,70 @@ Highcharts.chart('whasAppChart', {
 //Grafico de Pizza
 Highcharts.chart('pizzaChart', {
   chart: {
-      type: 'pie',
-      
+    type: 'pie'
   },
   title: {
-      text: 'Brasil',
-      style: {
-          fontSize: '18px'
-      }
+    text: 'Brasil',
+    style: {
+      fontSize: '18px'
+    }
   },
   tooltip: {
-      style: {
-          fontSize: '12px'
-      },
-      pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    style: {
+      fontSize: '12px'
+    },
+    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
   },
   legend: {
-      itemStyle: {
-          fontSize: '10px' // Tamanho da fonte da legenda
-      }
+    itemStyle: {
+      fontSize: '10px' // Tamanho da fonte da legenda
+    }
   },
   plotOptions: {
-      pie: {
-          allowPointSelect: true,
-          cursor: 'pointer',
-          dataLabels: {
-              enabled: false,
-              format: '<b>{point.name}</b>: {point.percentage:.1f} %'
-          },
-          showInLegend: true,
-          colors: ['blue', 'green', 'red', 'yellow', 'purple'].map(color =>
-              Highcharts.color(color).brighten(-0.2).get()
-          )
-      }
+    pie: {
+      allowPointSelect: true,
+      cursor: 'pointer',
+      dataLabels: {
+        enabled: false,
+        format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+      },
+      showInLegend: true,
+      colors: ['blue', 'green', 'red', 'yellow', 'purple'].map(color =>
+        Highcharts.color(color).brighten(-0.2).get()
+      )
+    }
   },
-  series: [{
+  series: [
+    {
       name: 'Porcentagem',
       colorByPoint: true,
-      data: [{
-              name: 'Recebimentos de Mensagens',
-              y: 30,
-              color: 'blue'
-          },
-          {
-              name: 'Usuários Ativos',
-              y: 29,
-              color: 'green'
-          },
-          {
-              name: 'Reclamações Registradas',
-              y: 20,
-              color: 'red'
-          },
-          {
-              name: 'Solicitações Respondidas',
-              y: 15,
-              color: 'yellow'
-          },
-          {
-              name: 'Outros',
-              y: 6,
-              color: 'purple'
-          }
+      data: [
+        {
+          name: 'Recebimentos de Mensagens',
+          y: 30,
+          color: 'blue'
+        },
+        {
+          name: 'Usuários Ativos',
+          y: 29,
+          color: 'green'
+        },
+        {
+          name: 'Reclamações Registradas',
+          y: 20,
+          color: 'red'
+        },
+        {
+          name: 'Solicitações Respondidas',
+          y: 15,
+          color: 'yellow'
+        },
+        {
+          name: 'Outros',
+          y: 6,
+          color: 'purple'
+        }
       ]
-  }]
+    }
+  ]
 });
-
-  
-  
-
-  
