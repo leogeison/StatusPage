c19179bddb26b12e36c54b0b1a874f99fa5e63d9
Léo Geison <leogeisom@gmail.com>
2024-04-07 18:50:20 -0300

    global styles and header component added

--- StatusPage/src/App.jsx
+++ StatusPage/src/App.jsx
@@ -1,4 +1,19 @@
+
+import styles from './App.module.css';
+import { Header } from './Components/Header';
 import './global.css';
+
 export function App() {
-  return <h1>teste</h1>;
+  return (
+    <div className={styles.wrapper}>
+      <Header />
+      <div className="header-content">
+        
+        <div className="header-text">
+          <h3>Todo o serviço operacional</h3>
+          <p>Último incidente 5 dias atrás</p>
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ StatusPage/src/Components/Header.jsx
@@ -0,0 +1,4 @@
+import styles from "./Header.module.css";
+export function Header() {
+  return <h1>Status Alertrack</h1>;
+}
